892a7c0bf0c3dddf824ff6440e0f958b728663c0
Alexandr Milevskiy <skainetd@gmail.com>
2017-01-30 19:00:12 +0300

    fixed order by date and added sort by view

--- js/search.js
+++ js/search.js
@@ -1,4 +1,7 @@
-﻿// Upon loading, the Google APIs JS client automatically invokes this callback.
+﻿// current searched videos;
+var items;
+
+// Upon loading, the Google APIs JS client automatically invokes this callback.
 googleApiClientReady = function() {
   gapi.client.setApiKey('AIzaSyCAKghrx-iQarm9RfDWOH8ssToUzuiSyTo');
   gapi.client.load('youtube', 'v3', function() {
@@ -11,6 +14,19 @@ function handleAPILoaded() {
   $('#search-button').attr('disabled', false);
 }
 
+// get views of video
+function getViews(counter, videoId)
+{
+  $.getJSON('https://www.googleapis.com/youtube/v3/videos?part=statistics&id=' + videoId + '&key=AIzaSyCAKghrx-iQarm9RfDWOH8ssToUzuiSyTo', function(data) {
+      items[counter].views = data.items[ 0 ].statistics.viewCount;
+  });
+}
+
+// sort by view
+function compareViews(video1, video2) {
+  return video2.views - video1.views;
+}
+
 // Search for a specified string.
 function search() {
   var q = $('#query').val();
@@ -19,7 +35,7 @@ function search() {
     q: q,
     part: 'snippet, id',
     maxResults: 20,
-    order: 'viewCount'
+    order: 'date'
   });
 
   // name video, author and date
@@ -27,22 +43,42 @@ function search() {
     var str = JSON.stringify(response.result);
     console.log(str);
 
+    // clear container
     $('#search-cointainer-list').empty();
     var searchItems = response.result.items;
+
+    // create array for structure of video
+    items = [];
+    // counter of video
     var counter = 0;
     $.each(searchItems, function(index, item) {
-      counter++;
-      var title = item.snippet.title;
-      var author = item.snippet.channelTitle;
-      var date = item.snippet.publishedAt;
       var id = item.id.videoId;
-      $('#search-cointainer-list').append('<li class="search-item">'
-                                          + '<a class="search-item-title" href="#accordion-' + counter + '">'
-                                          + title + '</a><br />'
-                                          + author + '<br />'
-                                          + date + '<br />'
-                                          + '<div id="accordion-' + counter + '" class="search-item-content">' + playVideo(id) + '</div></li>'
-                                          );
+      // added structure in array
+      items[counter] = {title: item.snippet.title, author: item.snippet.channelTitle, date: item.snippet.publishedAt, id: id};
+      // get views for video
+      getViews(counter, id);
+      counter++;
     })
+
+    // little pause
+    setTimeout(function() {
+      // sorting by views and output video
+      items.sort(compareViews);
+      output();
+    }, 1000);
   });
+}
+
+function output()
+{
+  // output in html
+  for (var i = 0; i < items.length; i++) {
+    $('#search-cointainer-list').append('<li class="search-item">'
+                                        + '<a class="search-item-title" href="#accordion-' + (i + 1) + '">'
+                                        + items[i].title + '</a><br />'
+                                        + items[i].author + '<br />'
+                                        + items[i].date + '<br />'
+                                        + '<div id="accordion-' + (i + 1) + '" class="search-item-content">' + playVideo(items[i].id) + '</div></li>'
+                                        );
+  }
 }
